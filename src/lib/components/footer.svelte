7568d8ffd59114bc6bf296b36ba14edc0858cf92
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-04 19:51:53 -0500

    Add footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,21 @@
+<footer class="h-14 xl:h-24 flex items-center justify-center px-4 xl:px-6">
+  <span class="text-xs sm:text-sm text-center text-muted-foreground">
+    Based on
+    <a
+      class="font-medium underline underline-offset-4"
+      href="https://www.shadcn-svelte.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      shadcn-svelte
+    </a>. Customized by
+    <a
+      class="font-medium underline underline-offset-4"
+      href="https://olegpolin.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Oleg Polin
+    </a>.
+  </span>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { ModeWatcher } from 'mode-watcher';
-	import Header from '$lib/components/header.svelte';
+  import './layout.css';
+  import favicon from '$lib/assets/favicon.svg';
+  import { ModeWatcher } from 'mode-watcher';
+  import Header from '$lib/components/header.svelte';
+  import Footer from '$lib/components/footer.svelte';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <ModeWatcher defaultMode="dark"/>
 
-<Header />
+<div class="min-h-svh flex flex-col">
+  <Header />
 
-{@render children()}
+  <main class="flex-1 flex flex-col">
+    {@render children()}
+  </main>
+
+  <Footer />
+</div>
