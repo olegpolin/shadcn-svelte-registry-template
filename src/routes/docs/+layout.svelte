464f8e04f097ea0729eec77f8c2595fe21129815
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-19 09:54:05 -0400

    Add docs layout

---
+++ src/routes/docs/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import type { LayoutProps } from './$types';
+  import { page } from '$app/state';
+  import * as Sidebar from '$lib/registry/ui/sidebar';
+  import { sidebarNavLinks } from '$lib/utils/navigation';
+
+  let { children }: LayoutProps = $props();
+</script>
+
+<Sidebar.Provider class="px-2">
+  <Sidebar.Root
+    class="max-md:hidden h-[calc(100svh-7.5rem)] xl:h-[calc(100svh-10rem)] sticky top-14.25 z-30 overscroll-none bg-transparent"
+    collapsible="none"
+  >
+    <Sidebar.Content class="overflow-x-hidden px-2">
+      <div class="sticky h-8 -top-1 z-10 shrink-0 bg-linear-to-b from-background via-background/80 to-background/50 blur-xs"></div>
+      {#each sidebarNavLinks as group (group.title)}
+        <Sidebar.Group>
+          <Sidebar.GroupLabel class="font-medium text-muted-foreground">
+            {group.title}
+          </Sidebar.GroupLabel>
+          <Sidebar.GroupContent>
+            <Sidebar.Menu>
+              {#each group.links as link}
+                <Sidebar.MenuItem>
+                  <Sidebar.MenuButton class="h-7.5 w-fit text-[0.8rem] font-medium data-[active=true]:bg-accent" isActive={page.url.pathname === link.href}>
+                    {#snippet child({ props })}
+                      <a {...props} href={link.href}>
+                        <span class="absolute inset-0 flex w-full bg-transparent"></span>
+                        {link.title}
+                      </a>
+                    {/snippet}
+                  </Sidebar.MenuButton>
+                </Sidebar.MenuItem>
+              {/each}
+            </Sidebar.Menu>
+          </Sidebar.GroupContent>
+        </Sidebar.Group>
+      {/each}
+      <div class="sticky h-16 -bottom-1 z-10 shrink-0 bg-linear-to-t from-background via-background/80 to-background/50 blur-xs"></div>
+    </Sidebar.Content>
+  </Sidebar.Root>
+  {@render children()}
+</Sidebar.Provider>
