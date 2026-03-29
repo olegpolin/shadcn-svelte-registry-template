f2f9caffb5823e69b27942ca5f4ccbf026c03a72
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-29 12:56:28 -0400

    Add components list

---
+++ src/lib/components/components-list.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { componentLinks } from "$lib/utils/navigation";
+</script>
+
+<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-x-8 lg:gap-x-16 lg:gap-y-6 xl:gap-x-20">
+  {#each componentLinks as link (link.title)}
+    <a href={link.href} class="text-lg font-medium underline-offset-4 hover:underline md:text-base">
+      {link.title}
+    </a>
+  {/each}
+</div>
