3f13cfd858f19eb779fe0d7a7b0392438ea384ad
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-04-26 15:32:33 -0400

    Run formatting

--- src/lib/registry/examples/tooltip-demo.svelte
+++ src/lib/registry/examples/tooltip-demo.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import { buttonVariants } from "$lib/registry/ui/button/index.js";
-	import * as Tooltip from "$lib/registry/ui/tooltip/index.js";
+  import { buttonVariants } from "$lib/registry/ui/button/index.js";
+  import * as Tooltip from "$lib/registry/ui/tooltip/index.js";
 </script>
 
 <Tooltip.Provider>
-	<Tooltip.Root>
-		<Tooltip.Trigger class={buttonVariants({ variant: "outline" })}>Hover</Tooltip.Trigger>
-		<Tooltip.Content>
-			<p>Add to library</p>
-		</Tooltip.Content>
-	</Tooltip.Root>
+  <Tooltip.Root>
+    <Tooltip.Trigger class={buttonVariants({ variant: "outline" })}>Hover</Tooltip.Trigger>
+    <Tooltip.Content>
+      <p>Add to library</p>
+    </Tooltip.Content>
+  </Tooltip.Root>
 </Tooltip.Provider>
