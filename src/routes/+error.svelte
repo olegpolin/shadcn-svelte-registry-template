8472f5fd969778c5a2e0dbd028fe1937e9383633
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-03 16:17:35 -0400

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from '$app/state';
+  import * as Empty from '$lib/registry/ui/empty';
+  import { Button } from '$lib/registry/ui/button';
+</script>
+
+<Empty.Root>
+  <Empty.Header>
+    <Empty.Title class="text-4xl">{page.status} - {page.error?.message}</Empty.Title>
+  </Empty.Header>
+  <Empty.Content>
+    <Button href="/">Go Home</Button>
+  </Empty.Content>
+</Empty.Root>
