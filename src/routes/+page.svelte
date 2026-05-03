db5b2431ac5312fb87decaa0db21fda76aa4180c
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-03 10:41:49 -0400

    Add components demo section to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Seo from "$lib/components/seo.svelte";
   import HeroSection from "$lib/components/hero-section.svelte";
+  import RootComponents from "$lib/components/root-components.svelte";
 
   const title = 'The Foundation for your Design System';
   const description = 'A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.';
@@ -20,3 +21,7 @@
     href: '/docs/components'
   }}
 />
+
+<div class="3xl:max-w-screen-2xl mx-auto max-w-350 px-4 lg:px-8 pb-6">
+  <RootComponents />
+</div>
